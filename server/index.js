fda43a3de669523ac3be7299977dece82fcd0947
Ayush pandey <ayushpandey939@gmail.com>
2023-12-13 18:29:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', Router);
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
     Connection();
     console.log(`Server is running successfully on PORT http://localhost:${PORT}`)
 });
