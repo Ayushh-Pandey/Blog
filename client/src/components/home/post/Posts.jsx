fbf42df9b458ccc1b636cc2e1946de8fb7c6aaa6
leoversion <ayushpandey939@gmail.com>
2025-04-30 16:29:27 +0530

    added loader in data fetching

--- client/src/components/home/post/Posts.jsx
+++ client/src/components/home/post/Posts.jsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import { API } from '../../../service/api';
-import { Box, Grid } from '@mui/material';
+import { Box, CircularProgress, Grid } from '@mui/material';
 import Post from './Post';
 import { Link, useSearchParams } from 'react-router-dom';
 
 const Posts = () => {
     const [posts, setPosts] = useState([]);
-    
+    const [loading, setLoading] = useState(true);
     const [searchParams] = useSearchParams();
     const category = searchParams.get('category');
 
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true)
             let response = await API.getAllPosts({ category: category || '' });
             if (response.isSuccess) {
                 setPosts(response.data);
             }
+            setLoading(false)
         }
         fetchData();
     }, [category]);
@@ -23,16 +25,20 @@ const Posts = () => {
     return (
         <>
             {
-                posts?.length > 0 ? posts.map(post => (
+                loading ? <Box style={{ width: '80%', height: '50%', display: 'flex', justifyContent: 'center', alignItems: "center", flexDirection: 'column', rowGap: '10px',}}>
+                    Loading data...
+                    <CircularProgress size={50} />
+                </Box> :
+                    posts?.length > 0 ? posts.map(post => (
 
-                    <Grid item lg={3} sm={4} sx={12}>
-                        <Link to={`details/${post._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <Post post={post} />
-                        </Link>
-                    </Grid>
-                )) : <Box style={{ color: '878787', margin: '30px 80px', fontsize: 18 }}>
-                    No data available to display
-                </Box>
+                        <Grid item lg={3} sm={4} sx={12}>
+                            <Link to={`details/${post._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                <Post post={post} />
+                            </Link>
+                        </Grid>
+                    )) : <Box style={{ color: '#878787', margin: '30px 80px', fontsize: 18 }}>
+                        No data available to display
+                    </Box>
             }
         </>
     )
